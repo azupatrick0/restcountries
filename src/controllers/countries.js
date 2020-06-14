b6c5de915a0cb16b13ce413c4f667fc4e9c9fd17
Patrick Azu <azupatrick0@gmail.com>
2020-06-14 11:54:54 +0100

    fetch unique country

--- src/controllers/countries.js
+++ src/controllers/countries.js
@@ -1,17 +1,42 @@
 import dotenv from 'dotenv';
+import axios from 'axios';
 
 dotenv.config();
 
 class Countries {
-  static async fetchUniqueCountry(_req, res) {
-    return res.status(200).json({
-      status: 200,
-      data: {
-        message: 'Country returned successfully',
-        countries: []
-      }
-    });
+  static async fetchUniqueCountry(req, res) {
+    const name = req.query.name;
+
+    if (!name) {
+      return res.status(400).json({
+        status: 400,
+        data: {
+          error: 'please enter country name'
+        }
+      });
+    } else {
+      const countries = await axios.get(`${process.env.API_URL}/name/${name}`);
+
+      let returnedCountries = [];
+
+      countries.data.map(country => {
+        returnedCountries = [...returnedCountries, {
+          name: country.name,
+          flag: country.flag,
+          region: country.region
+        }]
+      });
+
+      return res.status(200).json({
+        status: 200,
+        data: {
+          message: 'Countries returned successfully',
+          countries: returnedCountries
+        }
+      });
+    }
   }
 }
 
 export default Countries;
+
--- src/tests/index.js
+++ src/tests/index.js
@@ -9,7 +9,7 @@ describe('RestCountries Test Suite', () => {
   describe('GET countries/fetch - fetch a unique country', () => {
     it('should return status code 200 on fetching a unique country', async () => {
       const res = await chai.request(app)
-        .get('/api/v1/countries/fetch');
+        .get('/api/v1/countries/fetch?name=united');
       
       res.status.should.equal(200);
       res.body.should.be.a('object');
@@ -17,7 +17,9 @@ describe('RestCountries Test Suite', () => {
       res.body.should.have.property('data');
       res.body.data.should.have.property('message');
       res.body.data.should.have.property('countries');
-      res.body.data.message.should.equal('Country returned successfully');
+      res.body.data.message.should.equal('Countries returned successfully');
+      res.body.data.countries.length.should.be.gt(0);
+      res.body.data.countries[0].name.should.equal('United States Minor Outlying Islands');
     });
   });
 });
